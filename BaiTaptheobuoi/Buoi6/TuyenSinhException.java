0252f014bcc7cbe1d6441ab104af0fb97ee3644a
phamngocbach05 <phamngocbach05@gmail.com>
2026-07-28 19:30:32 +0700

    Buoi3: SoDoLop_Buoi3.drawio (UML class diagram) | Buoi6: Exception classes + SoDoLop_Buoi6.drawio

---
+++ BaiTaptheobuoi/Buoi6/TuyenSinhException.java
@@ -0,0 +1,12 @@
+
+
+// Exception co so cho toan bo he thong Quan ly Tuyen sinh
+public class TuyenSinhException extends Exception {
+    public TuyenSinhException(String message) {
+        super(message);
+    }
+
+    public TuyenSinhException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
